bf0d49c66eb18e939d47cd239d2c7127b427a22d
npc <1700969761@qq.com>
2023-03-04 19:31:32 +0800

    数据基本展示 over

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="app">
+    <!-- head part -->
+    <div style="height: 60px;line-height: 60px;background-color: white;margin-bottom: 2px;">
+      <img src="@/assets/logo.png" alt="logo" style="width: 60px;height: 60px;">
+      <span style="margin-left: 10px; font-size: 24px;">XP管理</span>
+    </div>
 
-    <router-view />
+    <!-- left prat and main -->
+    <div style="display: flex;">
+      <!-- 侧边栏导航 -->
+      <div style="width: 200px;min-height: calc(100vh-62px); overflow: hidden;margin-right: 2px;background-color: white;">
+        <el-menu :default-active="$route.path" :default-opends="['/']" router class="el-menu-demo">
+          <el-menu-item index="/">
+            <i class="el-icon-eleme">
+              <span>首页</span>
+            </i>
+          </el-menu-item>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-question">
+                <span>关于页面</span>
+              </i>
+            </template>
+            <el-menu-item index="about">关于详情</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+      <!-- 主题数据 -->
+      <div style="flex: 1;background-color: white;">
+        <router-view />
+      </div>
+    </div>
+
+
   </div>
 </template>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <el-button>按钮</el-button>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="name" label="Date" width="180" />
+      <el-table-column prop="age" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+      <el-table-column prop="phone" label="phone" />
+    </el-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      tableData: [
+        { name: "王一", age: 20, address: '北京市', phone: "110" },
+        { name: "王二", age: 20, address: '北京市', phone: "110" },
+        { name: "王三", age: 20, address: '北京市', phone: "110" },
+        { name: "王四", age: 20, address: '北京市', phone: "110" },
+      ]
+    }
   }
 }
 </script>
